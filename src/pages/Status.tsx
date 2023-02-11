273145592cd0c2ca1f24c527900d07c6c8f85bc1
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-10 21:37:51 -0300

    status anwser function

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react'
 import { Header } from '../components/Header'
 import { Separator } from '../components/Separator'
 import { Tweet } from '../components/Tweet'
@@ -9,6 +10,19 @@ export function Status() {
     'Faz sentido até.',
     'Parabéns pelo progresso!'
   ]
+
+  const [newAwnser, setNewAnwser] = useState('')
+  const [answers, setAnswers] = useState([
+    'Concordo...',
+    'Faz sentido até.',
+    'Parabéns pelo progresso!'
+  ])
+
+  function createNewAwnser(event: FormEvent) {
+    event.preventDefault()
+    setAnswers([newAwnser, ...answers])
+    setNewAnwser('')
+  }
   return (
     <main className="status">
       <Header title="Tweet" />
@@ -16,13 +30,19 @@ export function Status() {
       <Tweet content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione quisquam nostrum debitis beatae quia assumenda id deleniti earum, fuga labore quasi reiciendis amet sit impedit dolore minima laborum voluptatum. A." />
 
       <Separator />
-      <form className="answerTweetForm">
+      <form onSubmit={createNewAwnser} className="answerTweetForm">
         <label htmlFor="tweet">
           <img
             src="https://avatars.githubusercontent.com/u/92648265?v=4"
             alt="imagem do usuario"
           />
-          <textarea id="tweet" placeholder=" Tweet your awnser " />
+          <textarea
+            id="tweet"
+            placeholder=" Tweet your awnser "
+            onChange={event => {
+              setNewAnwser(event.target.value)
+            }}
+          />
         </label>
 
         <button type="submit">Awnser</button>
